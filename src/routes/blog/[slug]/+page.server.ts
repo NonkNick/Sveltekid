9653abd4a114d094699ad29258cf29d17131ba82
NonkNick <honossillie@gmail.com>
2026-05-29 17:39:00 +0200

    join op de db enzo. slug pages voor blogposts. echte dudes zouden het cachen maar nee

--- src/routes/blog/[slug]/+page.server.ts
+++ src/routes/blog/[slug]/+page.server.ts
@@ -1,6 +1,6 @@
 import type { PageServerLoad } from "./$types";
+import { getPostBySlug } from "$lib/server/queries/posts";
 
-export const load: PageServerLoad = async () => {
-	const post = await
-	return {  };
+export const load: PageServerLoad = async ({params}) => {
+	return { post: await getPostBySlug(params.slug)};
 };
--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
 	import BlogPost from "$lib/components/BlogPost.svelte";
+	import type { PageProps } from "./$types";
 
+	let { data }: PageProps = $props();
+	const { post } = data;
 </script>
-<BlogPost />
+
+<BlogPost {post}/>
